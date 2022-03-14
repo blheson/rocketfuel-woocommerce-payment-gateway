1f1ac67a5c418cba948288b0edfb5e0bf1c8755c
Udor Blessingm <berachah54@gmail.com>
2022-03-14 10:29:29 +0100

    Replace overwritten subscription class

--- Controllers/Rocketfuel_Gateway_Subscription_Controller.php
+++ Controllers/Rocketfuel_Gateway_Subscription_Controller.php
@@ -2,323 +2,109 @@
 
 namespace Rocketfuel_Gateway\Controllers;
 
-use Rocketfuel_Gateway\Plugin;
-
-class Rocketfuel_Gateway_Controller extends \WC_Payment_Gateway
+if (!defined('ABSPATH')) {
+	exit;
+}
+/**
+ * Class Rocketfuel_Gateway_Subscription_Controller 
+ */
+class Rocketfuel_Gateway_Subscription_Controller extends Rocketfuel_Gateway_Controller
 {
+
+	/**
+	 * Constructor
+	 */
 	public function __construct()
 	{
-		$this->id = 'rocketfuel_gateway';
+		parent::__construct();
+	
 
-		// $this->icon = 'rkfl.png';
+		// if (class_exists('WC_Subscriptions_Order')) {
 
-		$this->has_fields = false;
-
-		$this->method_title = 'Rocketfuel';
-
-		$this->method_description = 'Pay with Crypto using Rocketfuel';
-
-		$this->init_form_fields();
-
-		$this->init_settings();
-
-		$this->title = $this->get_option('title');
-
-
-		$this->environment = $this->get_option('environment');
-
-		$this->endpoint = $this->get_endpoint($this->environment);
-
-		$this->public_key = $this->get_option('public_key');
-
-		$this->description = $this->get_option('description');
-
-		$this->password = $this->get_option('password');
-
-		$this->email = $this->get_option('email');
-
-		$this->payment_complete_order_status = $this->get_option('payment_complete_order_status') ? $this->get_option('payment_complete_order_status') : 'completed';
-
-		$this->supports = array('products');
-
-		$this->merchant_id = $this->get_option('merchant_id');
-
-		add_action('woocommerce_update_options_payment_gateways_' . $this->id, array($this, 'process_admin_options'));
+		// 	add_action('woocommerce_scheduled_subscription_payment_' . $this->id, array($this, 'scheduled_subscription_payment'), 10, 2);
+		// }
 	}
-	public function get_endpoint($environment){
-		
-		$environment_data = array(
-			'prod' => 'https://app.rocketfuelblockchain.com/api',
-			'dev' => 'https://dev-app.rocketdemo.net/api',
-			'stage2' => 'https://qa-app.rocketdemo.net/api',
-			'preprod' => 'https://preprod-app.rocketdemo.net/api',
-			'sandbox' => 'https://app-sandbox.rocketfuelblockchain.com/api',
 
-		);
-
-		return isset($environment_data[$environment]) ? $environment_data[$environment] : 'https://app.rocketfuelblockchain.com/api';
-	}
-	public function init_form_fields()
-	{
-		$all_wc_order_status = wc_get_order_statuses();
-
-		$this->form_fields = apply_filters('rocketfuel_admin_fields', array(
-			'enabled' => array(
-				'title' => __('Enable/Disable', 'rocketfuel-payment-gateway'),
-				'type' => 'checkbox',
-				'label' => __('Enable Rocketfuel', 'rocketfuel-payment-gateway'),
-				'default' => 'yes'
-			),
-			'title' => array(
-				'title' => __('Title', 'rocketfuel-payment-gateway'),
-				'type' => 'text',
-				'description' => __('This controls the title which the user sees during checkout.', 'rocketfuel-payment-gateway'),
-				'default' => __('Rocketfuel', 'rocketfuel-payment-gateway'),
-				'desc_tip'      => true,
-			),
-
-			'environment' => array(
-				'title' => __('Working environment', 'rocketfuel-payment-gateway'),
-				'type' => 'select',
-				'default' => 'prod',
-				'options' =>  array(
-					'prod' => 'Production',
-					'dev' => 'Development',
-					'stage2' => 'QA',
-					'preprod' => 'Pre-Production',
-					'sandbox' => 'Sandbox'
-				)
-			),
-			'description' => array(
-				'title' => __('Customer Message', 'rocketfuel-payment-gateway'),
-				'type' => 'textarea',
-				'default' => 'Pay for your order with RocketFuel'
-			),
-			'merchant_id' => array(
-				'title' => __('Merchant ID', 'rocketfuel-payment-gateway'),
-				'type' => 'text',
-				'default' => ''
-			),
-			'public_key' => array(
-				'title' => __('Public Key', 'rocketfuel-payment-gateway'),
-				'type' => 'textarea',
-				'default' => ''
-			), 'email' => array(
-				'title' => __('Email', 'rocketfuel-payment-gateway'),
-				'type' => 'text',
-				'default' => ''
-			), 'password' => array(
-				'title' => __('Password', 'rocketfuel-payment-gateway'),
-				'type' => 'password',
-				'default' => ''
-			),
-			'payment_complete_order_status' => array(
-				'title' => __('Order Status for Completed Payment', 'rocketfuel-payment-gateway'),
-				'type' => 'select',
-				'default' => 'wc-completed',
-				'options' =>  $all_wc_order_status
-			),
-			'callback_url' => array(
-				'title' => __('Callback URL', 'rocketfuel-payment-gateway'),
-				'type' => 'checkbox',
-				'label' => esc_url(rest_url() . Plugin::get_api_route_namespace() . '/payment'),
-				'description' => __('Callback URL for Rocketfuel', 'rocketfuel-payment-gateway'),
-				'default' => '',
-				'css' => 'display:none'
-				// 'desc_tip'      => true,
-			)
-		));
-	}
 	/**
-	 * Parse cart items and prepare for order
-	 * @param array $items 
-	 * @return array
+	 * Check if an order has a subscription.
+	 *
+	 * @param int $order_id WC Order ID.
+	 *
+	 * @return bool
 	 */
-	public function sort_cart($items)
+	public function order_has_subscription($order_id)
 	{
-		$data = array();
-		foreach ($items as $cart_item) {
-			$data[] = array(
-				'name' => $cart_item['data']->get_title(),
-				'id' => (string)$cart_item['product_id'],
-				'price' => $cart_item['data']->get_price(),
-				'quantity' => (string)$cart_item['quantity']
-			);
-		}
 
-		if((null !== WC()->cart->get_shipping_total()) && (int)WC()->cart->get_shipping_total() > 0 && (!strpos(strtolower(WC()->cart->get_shipping_total()), 'free'))){
-
-			$data[] = array(
-				'name' => 'Shipping',
-				'id' => microtime(),
-				'price'=> WC()->cart->get_shipping_total(), 
-				'quantity' => '1'
-			);
-
-		}
-		
-		return $data;
+		return function_exists('wcs_order_contains_subscription') && (wcs_order_contains_subscription($order_id) || wcs_order_contains_renewal($order_id));
 	}
 	/**
-	 * Update order when payment has been confirmed
-	 * @param WP_REST_REQUEST $request_data
-	 * @return void
+	 * Process a subscription renewal.
+	 *
+	 * @param float    $amount_to_charge Subscription payment amount.
+	 * @param WC_Order $renewal_order Renewal Order.
 	 */
-	public function update_order($request_data){
+	public function scheduled_subscription_payment($amount_to_charge, $renewal_order)
+	{
 
-		$data = $request_data->get_params();
+		$response = $this->process_subscription_payment($renewal_order, $amount_to_charge);
 
-		if (!$data['order_id'] || !$data['status']) {
+		if (is_wp_error($response)) {
 
-			echo json_encode(array('status' => 'failed', 'message' => 'Order was not updated. Invalid parameter. You must pass in order_id and status'));
-
-			exit;
-
+			$renewal_order->update_status('failed', sprintf(__('Rocketfuel Transaction Failed (%s)', 'rocketfuel-payment-gateway'), $response->get_error_message()));
 		}
-
-		$order_id = $data['order_id'];
-
-		$status = $data['status'];
-
-		$order = wc_get_order($order_id);
-
-		if (!$order) {
-			echo json_encode(array('status' => 'failed', 'message' => 'Order was not updated. Could not retrieve order from the order_id that was sent'));
-			exit;
-		}
-
-		if ($status === 'admin_default')
-			$status = $this->payment_complete_order_status;
-
-		$data = $order->update_status($status);
-		echo json_encode(array('status' => 'success', 'message' => 'Order was updated'));
-		exit;
 	}
-
 	/**
-	 * Process payment and redirect user to payment page
-	 * @param int $order_id
-	 * @return false|array
+	 * Process a trial subscription order with 0 total.
+	 *
+	 * @param int $order_id WC Order ID.
+	 *
+	 * @return array|void
 	 */
 	public function process_payment($order_id)
 	{
-		global $woocommerce;
+
 		$order = wc_get_order($order_id);
-		$cart = $this->sort_cart(WC()->cart->get_cart());
 
-		$user_data = base64_encode(json_encode(array(
-			'first_name' => $order->get_billing_first_name(),
-			'last_name' => $order->get_billing_last_name(),
-			'email' => $order->get_billing_email(),
-			'merchant_auth' => 	$this->merchant_auth()
-		)));
-		try{
-							$shippingAddress = array(
-					"phoneNo"=> WC()->customer->get_shipping_phone(),
-					"address1"=> WC()->customer->get_shipping_address(),
-					"address2"=>  WC()->customer->get_shipping_address_2(),
-					"state"=>  WC()->customer->get_shipping_state(),
-					"city"=>  WC()->customer->get_shipping_city(),
-					"zipcode"=>  WC()->customer->get_shipping_postcode(),
-					"country"=> WC()->customer->get_shipping_country(),
-					"landmark"=> ""
-					
+		// Check for trial subscription order with 0 total.
+		if ($this->order_has_subscription($order) && $order->get_total() == 0) {
+
+			$order->payment_complete();
+
+			$order->add_order_note(__('This subscription has a free trial, hence the 0 amount', 'rocketfuel-payment-gateway'));
+
+			return array(
+				'result'   => 'success',
+				'redirect' => $this->get_return_url($order),
 			);
-		}catch(\Throwable $error){
-			
-	file_put_contents(__DIR__.'/error.json',json_encode( $error),FILE_APPEND);
+		} else {
+
+			return parent::process_payment($order_id);
 		}
-		$merchant_cred = array(
-			'email' => $this->email,
-			'password' => $this->password
-		);
-
-		$data = array(
-			'cred' => $merchant_cred,
-			'endpoint' => $this->endpoint,
-			'body' => array(
-				'amount' => $order->get_total(),
-				'cart' => $cart,
-				'merchant_id' => $this->merchant_id,
-		
-				'currency' => $order->get_currency(),
-				'order' => (string) $order_id,
-				'redirectUrl' => ''
-			)
-		);
-		file_put_contents(__DIR__.'/shipping.json',json_encode($shippingAddress),FILE_APPEND);
-if($shippingAddress){
-	$data['body']['shippingAddress'] = $shippingAddress ;
-}
-		
-		file_put_contents(__DIR__.'/log.json',json_encode($data),FILE_APPEND);
-
-		$payment_response = Process_Payment_Controller::process_payment($data);
-
-		if (!$payment_response) {
-			return;
-		}
-
-		$result = json_decode($payment_response);
-
-		if (!isset($result->result) && !isset($result->result->url)) {
-			wc_add_notice(__('Failed to place order', 'rocketfuel-payment-gateway'), 'error');
-			return false;
-		}
-		$urlArr = explode('/', $result->result->url);
-		$uuid = $urlArr[count($urlArr) - 1];
-
-		// Remove cart
-		$woocommerce->cart->empty_cart();
-		// Return thankyou redirect
-		// $pay_link = get_permalink(get_option(Plugin::$prefix . 'process_payment_page_id' ));
-		// $order_key = explode( 'order-received', $this->get_return_url($order))[1];
-		$buildUrl = $this->get_return_url($order) . "&uuid=" . $uuid . "&user_data=" . $user_data;
-
-		if ($this->environment !== 'prod') {
-
-			$buildUrl .= '&env=' . $this->environment;
-		}
-
-		return array(
-			'result' => 'success',
-			'redirect' => $buildUrl
-		);
 	}
-	public function merchant_auth()
-	{
-		return $this->get_encrypted($this->merchant_id);
-	}
+
 	/**
-	 * Encrypt Data
+	 * Process a subscription renewal payment.
 	 *
-	 * @param $to_crypt string to encrypt
-	 * @return string
+	 * @param WC_Order $order  Subscription renewal order.
+	 * @param float    $amount Subscription payment amount.
+	 *
+	 * @return bool|WP_Error
 	 */
-	public function get_encrypted($to_crypt)
+	public function process_subscription_payment($order, $amount)
 	{
+		return true; // do not allow yet
+		$order_id = method_exists($order, 'get_id') ? $order->get_id() : $order->id;
 
-		$out = '';
+		$auth_code  = null;
 
-		$pub_key_path = dirname(__FILE__) . '/rf.pub';
+		if ($auth_code) {
 
-		if (!file_exists($pub_key_path)) {
-			return false;
-		}
-		$cert = file_get_contents($pub_key_path);
+			$email = method_exists($order, 'get_billing_email') ? $order->get_billing_email() : $order->billing_email;
 
-		$public_key = openssl_pkey_get_public($cert);
+			$order_amount = $amount;
 
-		$key_lenght = openssl_pkey_get_details($public_key);
-
-		$part_len = $key_lenght['bits'] / 8 - 11;
-		$parts = str_split($to_crypt, $part_len);
-		foreach ($parts as $part) {
-			$encrypted_temp = '';
-			openssl_public_encrypt($part, $encrypted_temp, $public_key, OPENSSL_PKCS1_OAEP_PADDING);
-			$out .=  $encrypted_temp;
 		}
 
-		return base64_encode($out);
+		// return new WP_Error('rkfl_error', __('This subscription can&#39;t be renewed automatically. The customer will have to login to their account to renew their subscription', 'woo-paystack'));
 	}
 }
