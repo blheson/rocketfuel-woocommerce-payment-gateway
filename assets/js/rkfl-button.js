5c0bf79130f233e6431525a06413797a524137f3
Udor Blessingm <berachah54@gmail.com>
2022-07-12 13:17:35 +0100

    Save

--- assets/js/rkfl-button.js
+++ assets/js/rkfl-button.js
@@ -1,10 +1,29 @@
 (function ($, window, document) {
     'use strict';
     $(document).ready(() => {
+        let count = 0;
+        const intButn = setInterval(() => {
+            
+            try {
+                if ($('#place_order').attr('style').includes('display:none') && !$('#rocketfuel_retrigger_payment_button').attr('style')) {
+                    console.log("Force click triggered")
+                    $('form.checkout input.input-radio')[0]?.click(); //force click
+                }
+
+
+            } catch (error) {
+                console.error("Could not trigger place order", error?.message);
+            }
+            if(count>10){
+                console.log("RKFL - clear btn check");
+
+               clearInterval(intButn) 
+            }
+            count++;
+        }, 1000); 
+        // watch for 10secs
 
         $('form.checkout').on('click', 'input[name="payment_method"]', function () {
-            console.log("RKFL click is initiated")
-
             var isPPEC = $(this).is('#payment_method_rocketfuel_gateway');
             var toggleRKFL = isPPEC ? 'show' : 'hide';
             var toggleSubmit = isPPEC ? 'hide' : 'show';
@@ -21,18 +40,6 @@
                 padding: toggleSubmit
             }, 230);
         });
-        setTimeout(() => {
-            try {
-                if ($('#place_order').attr('style').includes('display:none') && !$('#rocketfuel_retrigger_payment_button').attr('style')) {
-                    $('form.checkout input.input-radio')[0]?.click(); //force click
-                }
-
-
-            } catch (error) {
-                console.error("Could not trigger place order", error?.message)
-            }
-        }, 500)
-
 
     })
 
